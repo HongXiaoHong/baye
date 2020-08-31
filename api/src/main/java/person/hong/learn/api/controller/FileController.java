ef3f5cb53872f5c535fda1e46f5c8cb437c30793
HongXiaoHong <1908711045@qq.com>
2020-09-01 00:23:33 +0800

    上传单个文件
    application.properties
    限制文件最大值 单个文件最大值

---
+++ api/src/main/java/person/hong/learn/api/controller/FileController.java
@@ -0,0 +1,45 @@
+package person.hong.learn.api.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * @description:
+ * @author: 洪晓鸿
+ * @time: 2020/9/1 0:11
+ */
+@Controller
+@RequestMapping("file")
+public class FileController {
+    /**
+     * @description 上传单个文件
+     * @param file
+     * @return
+     */
+    @PostMapping("/upload/alone")
+    @ResponseBody
+    public String upload(@RequestParam("file") MultipartFile file) {
+        if (file.isEmpty()) {
+            return "上传失败，请选择文件";
+        }
+
+        String fileName = file.getOriginalFilename();
+        String filePath = "D:/temp/picture/learn/";
+        File dest = new File(filePath + fileName);
+        try {
+            file.transferTo(dest);
+            System.out.println("上传成功");
+            return "上传成功";
+        } catch (IOException e) {
+            System.out.println(e.toString());
+        }
+        return "上传失败！";
+    }
+}
