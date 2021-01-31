84d0c621e35b3c21e4fdc78000459527ad940c07
HongXiaoHong <1908711045@qq.com>
2021-01-31 22:46:32 +0800

    gradle 学习分支创建

---
+++ gradle-learn/src/test/java/cn/gd/cz/hong/TestMain.java
@@ -0,0 +1,16 @@
+package cn.gd.cz.hong;
+
+import org.junit.Test;
+
+/**
+ * hello gradle
+ * @author hong
+ * 2021/1/31 22:35
+ */
+public class TestMain {
+
+    @Test
+    public void test() {
+        System.out.println("Hello Gradle");
+    }
+}
