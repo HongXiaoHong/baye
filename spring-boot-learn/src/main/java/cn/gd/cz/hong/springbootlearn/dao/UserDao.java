5ed2c9a95ed561e737d10f43df5590a07577e33e
HongXiaoHong <1908711045@qq.com>
2021-05-28 23:33:01 +0800

    userdao

--- spring-boot-learn/src/main/java/cn/gd/cz/hong/springbootlearn/dao/UserDao.java
+++ spring-boot-learn/src/main/java/cn/gd/cz/hong/springbootlearn/dao/UserDao.java
@@ -3,6 +3,8 @@ package cn.gd.cz.hong.springbootlearn.dao;
 import cn.gd.cz.hong.springbootlearn.entity.User;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 /**
  * 查询用户
  */
@@ -10,4 +12,5 @@ import org.apache.ibatis.annotations.Mapper;
 public interface UserDao {
 
     User selectById(String id);
+    List<User> selectUsers(String id);
 }
