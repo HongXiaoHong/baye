aebc1c93b00ad0dcac7a60cbbd6665911bbc0c52
HongXiaoHong <1908711045@qq.com>
2021-07-25 15:16:41 +0800

    thymeleaf demo

---
+++ spring-boot-thymeleaf-demo/src/test/java/cn/gd/cz/hong/springbootthymeleafdemo/SpringBootThymeleafDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.gd.cz.hong.springbootthymeleafdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootThymeleafDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
