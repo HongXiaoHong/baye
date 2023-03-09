e8936f539b5ce4b696c31e4bfc0bc5791e0a4e77
洪晓鸿 <1908711045@qq.com>
2023-03-09 22:05:30 +0800

    logback demo init

---
+++ logback/src/test/java/cn/gd/cz/hong/demo/logback/LogbackApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.gd.cz.hong.demo.logback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogbackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
