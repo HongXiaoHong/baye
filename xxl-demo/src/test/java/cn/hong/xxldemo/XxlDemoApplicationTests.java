5a503e821a8665a1d4b5dadd5eaac9a26e3e9620
洪晓鸿 <1908711045@qq.com>
2023-12-25 20:52:25 +0800

    xxl job 搭建(待续)

---
+++ xxl-demo/src/test/java/cn/hong/xxldemo/XxlDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.hong.xxldemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XxlDemoApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
